e9c95a10af2af30b944cadda4c9c60f30d2df169
Divya Ayikkara <divyaayikkara@nwk-69-111.nwk.ruw.rutgers.edu>
2018-02-01 16:29:22 -0500

    need to make it look pretty

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,30 +1,38 @@
 $(document).ready(function(){
 
-var topics = ["Harry Potter" , "Hermione Granger" , "Ron Weasley" , "Luna Lovegood" , "Ginny Weasley" , "Neville Longbottom"];
+var topics = ["Harry Potter" , "Ron Weasley" , "Hermione Weasley" , "Luna Lovegood" , "Ginny Weasley" , "Neville Longbottom" , "Albus Dumbledore" , "Sirius Black"];
 
-
-
-$("#submitButton").on("click", function() {
-	var input = $("#userInput").val().trim();
-	topics.push(input);
-	makeButton();
-	return false;
-})
 
 function displayGif() {
 	$("#displayImages").empty();
 	var input = $(this).attr("data-name");
 	//var limit = 10;
-	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + input + "&limit=10&api_key=rrDJESjwmjV9unpaBxu4sPqAkAK2Tyh5";
+	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + input + "&limit=20&api_key=rrDJESjwmjV9unpaBxu4sPqAkAK2Tyh5";
 	//console.log(queryURL);
 
 	$.ajax({
 		url: queryURL,
 		method: "GET"
 	}).done(function(response){
-		for(var i = 0; i < response.length; i++){
-			console.log(response);
+		
+		console.log(response);
+		var results = response.data;
+		for(var i = 0; i < results.length; i++){
+			var gifDiv = $("<div>");
+			var image = $("<img>");
+			image.attr("src", results[i].images.fixed_height_still.url); 
+            image.attr("data-still", results[i].images.fixed_height_still.url); 
+            image.attr("data-animate",results[i].images.fixed_height.url); 
+            image.attr("data-state", "still"); 
+            image.attr("class" , "gif");
+            gifDiv.append(image);
 
+            var title = $("<p>").text("Title: " + results[i].title); 
+			var rating = $("<p>").text("Rating: " + results[i].rating);
+			gifDiv.append(title);
+			gifDiv.append(rating);
+
+            $("#displayImages").prepend(gifDiv);
 		}
 	});
 }
@@ -34,7 +42,8 @@ function makeButton(){
 
 	for (var i = 0; i < topics.length; i++) {
 		var newButton = $("<button>");
-		newButton.addClass("btn btn-default");
+		newButton.attr("class" , "btn btn-default");
+		newButton.attr("id", "input");
 		newButton.attr("data-name" , topics[i]);
 		newButton.text(topics[i]);
 		$("#displayButtons").append(newButton);
@@ -42,16 +51,34 @@ function makeButton(){
 	}
 }
 
+function gifChange() {
+	var state = $(this).attr("data-state");
+        var animateImage = $(this).attr("data-animate");
+        var stillImage = $(this).attr("data-still");
 
+        if(state == "still") {
+            $(this).attr("src", animateImage);
+            $(this).attr("data-state", "animate");
+        }
 
+        else if(state == "animate") {
+            $(this).attr("src", stillImage);
+            $(this).attr("data-state", "still");
+        }   
+}
 
+$("#submitButton").on("click", function() {
+	var input = $("#userInput").val().trim();
+	form.reset();
+	topics.push(input);
+	makeButton();
+	return false;
+})
 
 makeButton();
-
-displayGif();
-
-
-})
+$(document).on("click" , "#input" , displayGif);
+$(document).on("click" , ".gif" , gifChange);
+});
 
 
 
